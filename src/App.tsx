61dcb744e5d43a8f376c5545860e46a638c85a8d
Vishal21121 <vs84077000@gmail.com>
2024-02-05 20:12:53 +0530

    Add react-icons and implement Todo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Signin from "./Pages/Signin";
-import Signup from "./Pages/Signup";
+// import Signin from "./Pages/Signin";
+// import Signup from "./Pages/Signup";
+import Todo from "./Pages/Todo";
 
 function App() {
   return (
     <div className="bg-gray-950 w-full h-screen">
       {/* <Signup /> */}
-      <Signin />
+      {/* <Signin /> */}
+      <Todo />
     </div>
   );
 }
---
+++ src/Pages/Todo.tsx
@@ -0,0 +1,35 @@
+import { ReactElement } from "react";
+import { FaPlusCircle } from "react-icons/fa";
+
+const Todo = (): ReactElement => {
+  return (
+    <div className="w-full p-8 flex justify-center h-screen">
+      {/* upper part */}
+      <div className="bg-gray-700 rounded-xl w-3/4 flex flex-col items-center mt-8">
+        <div className="flex w-1/2 ring-1 ring-gray-500 mx-auto justify-center gap-4 p-4 bg-gray-950 mt-8 rounded-2xl">
+          <div className="mr-8 mt-8">
+            <p className="text-2xl text-orange-200 font-bold">Todo Done</p>
+            <p className="text-white">Keep it up</p>
+          </div>
+          <div className="w-1/4 h-[130px] rounded-full bg-red-500  flex justify-center items-center">
+            <p className="text-2xl text-gray-900 font-extrabold">1/3</p>
+          </div>
+        </div>
+        <div className="flex border mt-8 w-1/2 justify-center p-4 gap-4">
+          <textarea
+            name=""
+            id=""
+            cols={30}
+            className="w-1/2 h-16 resize-none p-4 bg-gray-900 text-white rounded-2xl outline-none text-left"
+            placeholder="Write your next task"
+          ></textarea>
+          <button className="">
+            <FaPlusCircle className="text-3xl text-red-500" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
